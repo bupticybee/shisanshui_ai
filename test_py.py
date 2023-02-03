f88266a23d16e74a334f4346d8a04b379cff0e18
wangfuming <wangfuming@bytedance.com>
2023-02-03 16:42:27 +0800

    :new: add timing profile, less than 0.1s per decision

--- test_py.py
+++ test_py.py
@@ -2,6 +2,7 @@ import numpy as np
 import s3spy
 from S3sHelper import S3sHelper
 import random
+import time
 
 input_cards = np.asarray([0x12,0x23,0x33,0x44,0x35,0x25,0x28,0x29,0x39,0x3C,0x4C,0x2D,0x1C],dtype=np.uint8)
 
@@ -23,7 +24,10 @@ random.shuffle(cards)
 p1_cards = cards[:13]
 p2_cards = cards[13:26]
 
+start = time.time()
 strategy1, strategy2 = s3shelper.get_strategy(p1_cards,p2_cards)
+end = time.time()
+print("cfr used: ",end - start,"s")
 
 print("=" * 20 + "STRATEGY 1" + "=" * 20)
 for one_strategy in strategy1:
